5d4fccf2573d70607bd1727dec12e8d4bb9132cf
Lee Sole <lee_sole@yahoo.com>
2017-08-07 12:48:07 +0000

    Removed old layout, added new, started work on system proper

--- Distributor/App_Start/BundleConfig.cs
+++ Distributor/App_Start/BundleConfig.cs
@@ -20,50 +20,14 @@ namespace Distributor
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/js/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/distributor").Include(
-                        "~/Scripts/metisMenu*",
-                        "~/Scripts/sb-admin-2*",
-                        "~/Scripts/datatables/datatables*",
-                        "~/Scripts/datatables/jquery*",
-                        "~/Scripts/datatables-plugins/datatables*",
-                        "~/Scripts/datatables-responsive/datatables*",
-                        "~/Scripts/morris/morris*",
-                        "~/Scripts/raphael/raphael*"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/css/bootstrap.css",
+                      "~/css/bootstrap-theme.css",
+                      "~/css/modern-business.css",
                       "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/Distributorcss").Include(
-                      "~/Content/font-awesome.css",
-                      "~/Content/metisMenu.css",
-                      "~/Content/sb-admin-2.css",
-                      "~/Content/datatables/datatables*",
-                      "~/Content/datatables/jquery*",
-                      "~/Content/datatables-plugins/datatables*",
-                      "~/Content/datatables-responsive/datatables*",
-                      "~/Content/morris/morris*"));
-
-            bundles.Add(new StyleBundle("~/Contect/SCSS").Include(
-                      "~/Content/SCSSStyles/font-awesome/scss/_animated.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_bordered-pulled.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_core.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_extras.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_fixed_width.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_icons.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_larger.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_list.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_mixins.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_path.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_rotated-flipped.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_screen-reader.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_spinning.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_stacked.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/_variables.scss",
-                      "~/Content/SCSSStyles/font-awesome/scss/font-awesome.scss"));
         }
     }
 }
---
+++ Distributor/Enums/EntityEnums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Distributor.Enums
+{
+    public static class EntityEnums
+    {
+        public enum EntityStatus
+        {
+            Inactive = 0,
+            Active = 1
+        }
+    }
+}
