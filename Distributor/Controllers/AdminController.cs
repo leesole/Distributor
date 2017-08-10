90bf9af5feafecfe3751b14956bc06d53fcae905
Lee Sole <lee_sole@yahoo.com>
2017-08-10 19:32:56 +0000

    Admin screens added

---
+++ Distributor/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Distributor.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public ActionResult Tasks()
+        {
+            return View();
+        }
+
+        public ActionResult UserAdmin()
+        {
+            return View();
+        }
+
+        public ActionResult BranchAdmin()
+        {
+            return View();
+        }
+        public ActionResult CompanyAdmin()
+        {
+            return View();
+        }
+    }
+}
