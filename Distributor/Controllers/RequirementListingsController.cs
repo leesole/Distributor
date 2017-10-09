2ccd7021850eee6c213cec5642faf54e7b3a1c84
Lee Sole <lee.sole@capita.co.uk>
2017-10-09 10:58:06 +0000

    commit

--- Distributor/Controllers/RequirementListingsController.cs
+++ Distributor/Controllers/RequirementListingsController.cs
@@ -138,7 +138,7 @@ namespace Distributor.Controllers
 
         public ActionResult History()
         {
-            List<RequirementListingManageView> model = RequirementListingManageHelpers.GetAllRequirementListingsManageView(User, false);
+            List<RequirementListingManageView> model = RequirementListingManageHelpers.GetAllRequirementListingsManageView(User, true);
             return View(model);
         }
 
